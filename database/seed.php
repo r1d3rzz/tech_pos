842615cb2e4e69101231c0666a412f0c2584db81
r1d3rzz <myintthwaykhine127@gmail.com>
2023-05-26 18:31:06 +0630

    fetch products data and create productindex ui

--- database/seed.php
+++ database/seed.php
@@ -7,7 +7,7 @@ query("delete from category");
 
 query('alter table category auto_increment=1');
 
-$categories = ['Hat', 'Shirt', 'Shoe', 'Toys'];
+$categories = ['Hat', 'Shirt', 'Shoe', 'Toys', 'Hats'];
 
 foreach ($categories as $category) {
     query("insert into category (slug,name) values (?,?)", [
@@ -17,4 +17,42 @@ foreach ($categories as $category) {
 }
 // --------------------------------------------------------------------------
 
+
+//for Product
+
+query("delete from product");
+
+query("alter table product auto_increment=1");
+
+$products = [
+    ['category_id' => 1, 'slug' => slug('slug'), 'name' => 'A', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 1, 'slug' => slug('slug'), 'name' => 'A', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 1, 'slug' => slug('slug'), 'name' => 'A', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 2, 'slug' => slug('slug'), 'name' => 'B', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 2, 'slug' => slug('slug'), 'name' => 'B', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 2, 'slug' => slug('slug'), 'name' => 'B', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 3, 'slug' => slug('slug'), 'name' => 'C', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 3, 'slug' => slug('slug'), 'name' => 'C', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 3, 'slug' => slug('slug'), 'name' => 'C', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 4, 'slug' => slug('slug'), 'name' => 'D', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 4, 'slug' => slug('slug'), 'name' => 'D', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 4, 'slug' => slug('slug'), 'name' => 'D', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 5, 'slug' => slug('slug'), 'name' => 'E', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 5, 'slug' => slug('slug'), 'name' => 'E', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+    ['category_id' => 5, 'slug' => slug('slug'), 'name' => 'E', 'image' => 'img', 'description' => 'This is Description', 'total_quantity' => 10, 'sale_price' => 100],
+];
+
+foreach ($products as $product) {
+    query("insert into product (category_id,slug,name,image,description,total_quantity,sale_price) values (?,?,?,?,?,?,?)", [
+        $product['category_id'],
+        $product['slug'],
+        $product['name'],
+        $product['image'],
+        $product['description'],
+        $product['total_quantity'],
+        $product['sale_price'],
+    ]);
+}
+
+// --------------------------------------------------------------------------
 echo "Seeded Successful";
--- product/index.php
+++ product/index.php
@@ -3,6 +3,8 @@ require "../init.php";
 
 if (!$_SESSION['user']) require redirect("/");
 
+$products = getAll("select * from product order by id desc");
+
 ?>
 
 <?php require "../include/header.php"; ?>
@@ -13,6 +15,61 @@ if (!$_SESSION['user']) require redirect("/");
             <div class="card-body">
                 <div class="mb-2">Product > <small class="badge bg-primary">All</small></div>
                 <a href="<?= $root . "product/create.php"; ?>" class="btn btn-sm btn-outline-dark">Create Product</a>
+
+
+                <div class="my-2">
+                    <?php showMsg(); ?>
+                    <?php showError(); ?>
+                </div>
+
+                <table class="table">
+                    <thead>
+                        <?php if (count($products)) { ?>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Sale Price</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        <?php } ?>
+                    </thead>
+                    <tbody id="tblData">
+                        <?php
+
+                        if (count($products)) { ?>
+
+                            <?php foreach ($products as $product) : ?>
+                                <tr>
+                                    <td><?= $product->name ?></td>
+                                    <td><?= $product->total_quantity ?></td>
+                                    <td><?= $product->sale_price ?></td>
+                                    <td class="btn-group">
+                                        <a href="<?= $root . "product/edit.php?action=edit&slug=$product->slug"; ?>" class="btn btn-sm btn-primary">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                        <a onclick="return confirm('Are your sure want to delete it?')" href="<?= $root . "product/delete.php?action=delete&slug=$product->slug"; ?>" class="btn btn-sm btn-danger">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </a>
+                                        <a href="<?= $root . "product/edit.php?action=edit&slug=$product->slug"; ?>" class="btn btn-sm btn-warning">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <span class="mx-2 fw-bold">|</span>
+                                        <a href="<?= $root . "product/edit.php?action=edit&slug=$product->slug"; ?>" class="btn btn-sm btn-outline-danger">
+                                            Buy
+                                        </a>
+                                        <a href="<?= $root . "product/edit.php?action=edit&slug=$product->slug"; ?>" class="btn btn-sm btn-outline-primary">
+                                            Sale
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+
+                        <?php } else { ?>
+                            <div class="alert alert-warning mt-3">Empty Product</div>
+                        <?php } ?>
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
